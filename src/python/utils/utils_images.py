595604a652164b3d6f3a21400a098c5839aaf535
NetanelBlumenfeld <110423835+NetanelBlumenfeld@users.noreply.github.com>
2023-12-25 15:03:43 +0200

    2

--- src/python/utils/utils_images.py
+++ src/python/utils/utils_images.py
@@ -15,7 +15,7 @@ def normalize_img(img: np.ndarray, type: Normalization) -> np.ndarray:
     if type == Normalization.NONE:
         return img
     elif type == Normalization.Range_0_1:
-        return (img - np.min(img)) / (np.max(img) - np.min(img))
+        return (img - np.min(img)) / (np.max(img) - np.min(img) + EPSILON)
     elif type == Normalization.Range_neg_1_1:
         return (img - np.min(img)) / (np.max(img) - np.min(img) + EPSILON) * 2 - 1
     else:
